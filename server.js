1763c59d62bea81e50978f6907f418cb4fd0946d
sarahwylie <sarahannemariawylie@gmail.com>
2022-01-16 14:23:30 -0600

    defined id parameter

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
@@ -41,7 +40,12 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
-}
+};
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -49,6 +53,14 @@ app.get('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+app.get('/api/animals/:id', (req, res) => {
+const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
