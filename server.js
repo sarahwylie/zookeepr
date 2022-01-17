4247f4389a7baa3f50d0e862dad39082e4d8f893
sarahwylie <sarahannemariawylie@gmail.com>
2022-01-16 20:20:30 -0600

    adding insomnia

--- server.js
+++ server.js
@@ -62,6 +62,11 @@ const result = findById(req.params.id, animals);
         res.send(404);
     }
 });
+app.post('/api/animals', (req, res) => {
+  // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(rez.body);
+});
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
