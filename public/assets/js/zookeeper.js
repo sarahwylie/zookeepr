395e0c87499e44908fe51d9dc4be016e67c703fb
sarahwylie <sarahannemariawylie@gmail.com>
2022-01-19 14:27:27 -0600

    finished-ish

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,3 +1,5 @@
+// const { query } = require("express");
+const $zookeeperForm = document.querySelector("#zookeeper-form");
 const $displayArea = document.querySelector('#display-area');
 
 const printResults = resultArr => {
@@ -20,7 +22,13 @@ const printResults = resultArr => {
   $displayArea.innerHTML = animalHTML.join('');
 };
 
-const getZookeepers = () => {
+const getZookeepers = (formData = {}) => {
+  let queryUrl = '/api/zookeepers?';
+
+  Object.entries(formData).forEach(([key, value]) => {
+    queryUrl +=`${key}=${value}&`;
+  });
+
   fetch('/api/zookeepers')
     .then(response => {
       if (!response.ok) {
@@ -34,4 +42,18 @@ const getZookeepers = () => {
     });
 };
 
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeeperObj = { name, age };
+
+  getZookeepers(zookeeperObj);
+};
+
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 getZookeepers();
